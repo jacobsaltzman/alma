f8a35de48d77177dcba855b7642894e19fa70702
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2022-08-30 00:04:53 -0700

    Adds react-router v5 and imports functionality into index.js and app.js

--- alma-app/src/App.js
+++ alma-app/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Switch>
+        <Route>
+
+        </Route>
+        
+      </Switch>
     </div>
   );
 }
