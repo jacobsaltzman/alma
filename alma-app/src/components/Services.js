055fedb7ec8cbab5a14e47eb3671716c7537c06a
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2022-10-04 03:39:12 -0700

    Adds all the pictures and creates p tags for About sections

--- alma-app/src/components/Services.js
+++ alma-app/src/components/Services.js
@@ -3,7 +3,16 @@ import React from "react";
 function Services(){
 
   return (
-    <p>Bitters and Sharps services</p>
+    <div>
+    <h1>Bitters and Sharps services</h1>
+    <p>We provide custom grafting, orchard planning, and consultation services. Apple sales and scion wood sales are also available.
+We source certified virus free rootstock regionally, from a third generation family rootstock farm. Our scion wood is collected from our own orchard as well as from local orchard partners. 
+In winter, when the fruit trees are dormant, we are busy grafting. We take the time to select rootsock and scion pairs and then graft them by hand. The union is wrapped and sealed with care and trees are ready to plant in early spring. We typically graft about 1,000 trees per season and are happy to graft a few trees for your backyard or enough to fill your orchard.
+    </p>
+    <p>
+    Information on specific cultivars can be provided upon request. Send us an email with your objectives and we can work with you to create a plan to best meet your needs. We also offer full orchard consultations services from orchard planning, installation, maintenance, to integrated pest management (IPM) solutions and spray schedules for organic and conventional orcharding.
+    </p>
+    </div>
   )
 }
 
