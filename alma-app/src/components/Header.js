0bc1f097a888770bdb96612625f560909fd68031
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2022-10-29 22:30:37 -0700

    Style changes.

--- alma-app/src/components/Header.js
+++ alma-app/src/components/Header.js
@@ -7,11 +7,13 @@ function Header(){
     <header>
       <div className="navbar">
         <h1>Alma Cider</h1>
+        <div id="nav-links">
         <Link to="/">Home</Link>
         <Link to="">Ciders</Link>
         <Link to="/services">Services</Link>
         <Link to="">Retailers</Link>
         <Link to="/about">About Us</Link>
+        </div>
       </div>
     </header>
   )
