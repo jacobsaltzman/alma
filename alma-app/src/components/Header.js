3416f9ade80e011f8ac9c5c112de77832a2171cb
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2022-11-16 03:20:00 -0800

    Adds the banner and logo on the homepage and header and centers them.

--- alma-app/src/components/Header.js
+++ alma-app/src/components/Header.js
@@ -1,19 +1,24 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import banner from '/home/ickus/Development/code/alma/alma-app/src/images/banner.png';
 
 function Header(){
+
+  const image = <img alt="banner" src={banner} id='banner-logo'></img>
 
   return(
     <header>
       
       <div className="navbar">
         <div id="nav-links">
-        <Link to="/">Home</Link>
+        
         <Link to="/ciders">Ciders</Link>
         <Link to="/services">Services</Link>
         <Link to="">Retailers</Link>
         <Link to="/about">About Us</Link>
+        <Link to="/">{image}</Link>
         </div>
+
       </div>
     </header>
   )
--- alma-app/src/components/Home.js
+++ alma-app/src/components/Home.js
@@ -1,9 +1,14 @@
 import React from "react";
+import logo from '/home/ickus/Development/code/alma/alma-app/src/images/logo_large.png'
+
+
+console.log(logo)
 
 function Home(){
   return (
     <section id="home">
-      <h1>Alma Cider</h1>
+      <div id="main-logo-div">well, hello</div>
+      <img id="main-logo-image" alt="Alma Logo" src={logo} />
     </section>
   )
 }
