10a92e263caaa6f5b3f3a3f7cd07c24b5ba868b6
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2022-12-29 01:38:30 -0800

    Builds out the list format for adding the ciders and details on the ciders component page.

--- alma-app/src/components/Ciders.js
+++ alma-app/src/components/Ciders.js
@@ -5,7 +5,29 @@ function Ciders(){
 
   return(
     <div>
-    <p>Ciders go here.</p>
+      <div className="cider-list">
+        <div className="cider-list-items">
+          <h5 className="cider-name">Cider Name</h5>
+          <p className="flavors">flavor/style in italics</p>
+          <p className="cider-award-year">awards and year of win</p>
+        </div>
+        <div className="cider-list-items">
+          <h5 className="cider-name">Cider Name</h5>
+          <p className="flavors">flavor/style in italics</p>
+          <p className="cider-award-year">awards and year of win</p>
+        </div>
+        <div className="cider-list-items">
+          <h5 className="cider-name">Cider Name</h5>
+          <p className="flavors">flavor/style in italics</p>
+          <p className="cider-award-year">awards and year of win</p>
+        </div>
+        <div className="cider-list-items">
+          <h5 className="cider-name">Cider Name</h5>
+          <p className="flavors">flavor/style in italics</p>
+          <p className="cider-award-year">awards and year of win</p>
+        </div>
+      </div>
+    
     <p>Might add awards here or give it its own component and page</p>
     </div>
   )
