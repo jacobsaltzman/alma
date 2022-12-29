fbc064cbdedb92744bf5d50a224213b5e3303d11
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2022-12-29 00:36:06 -0800

    Adds additional background and changes some styling in the footer.

--- alma-app/src/components/Footer.js
+++ alma-app/src/components/Footer.js
@@ -10,21 +10,23 @@ function Footer(){
   return(
     <section id="footer">
       <div id="contact-us">
-        <p>   Contact us: <br/>
-              Email: info@almacider.com<br/>
-              Ph: 425.312.5734
-        </p>
+           <h4>Contact</h4>
+           <p>Mount Vernon, WA <br/>
+            info@almacider.com <br/>
+            425.312.5734</p>
       </div>
       <div id="partners">
         
       </div>
       <div id="social-media">
-        <SocialIcon url="https://www.facebook.com/almaciderco/" style={{ height: 25, width: 25 }}/>
-        <SocialIcon url="https://www.instagram.com/almaciderco/" style={{ height: 25, width: 25 }} />
-        <SocialIcon network="mailto" url="info@almacider.com" style={{ height: 25, width: 25 }} />
-        <SocialIcon url="https://www.youtube.com/watch?v=GPmrh-3Xdi0&ab_channel=CiderChat" style={{ height: 25, width: 25 }} />
-        <SocialIcon url="https://twitter.com/AlmaCider" style={{ height: 25, width: 25 }} />
-
+      <h4>Connect</h4>
+        <div className="icons">
+        <SocialIcon url="https://www.facebook.com/almaciderco/" style={{ height: 20, width: 20 }}/>
+        <SocialIcon url="https://www.instagram.com/almaciderco/" style={{ height: 20, width: 20 }} />
+        <SocialIcon network="mailto" url="info@almacider.com" style={{ height: 20, width: 20 }} />
+        <SocialIcon url="https://www.youtube.com/watch?v=GPmrh-3Xdi0&ab_channel=CiderChat" style={{ height: 20, width: 20 }} />
+        <SocialIcon url="https://twitter.com/AlmaCider" style={{ height: 20, width: 20 }} />
+        </div>
       </div>
     </section>
   )
