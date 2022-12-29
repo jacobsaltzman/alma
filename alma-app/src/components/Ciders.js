df6f4a621b8be5ac86ee945933b483a5b65431c7
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2022-12-29 01:57:04 -0800

    Adds centered headers for both sections and italisizes the flavor class.

--- alma-app/src/components/Ciders.js
+++ alma-app/src/components/Ciders.js
@@ -4,31 +4,32 @@ function Ciders(){
 
 
   return(
-    <div>
+    <div id="ciders-component">
+      <h3>Ciders</h3>
       <div className="cider-list">
         <div className="cider-list-items">
           <h5 className="cider-name">Cider Name</h5>
           <p className="flavors">flavor/style in italics</p>
-          <p className="cider-award-year">awards and year of win</p>
+          <p className="cider-award-year">*awards and year of win</p>
         </div>
         <div className="cider-list-items">
           <h5 className="cider-name">Cider Name</h5>
           <p className="flavors">flavor/style in italics</p>
-          <p className="cider-award-year">awards and year of win</p>
+          <p className="cider-award-year">*awards and year of win</p>
         </div>
         <div className="cider-list-items">
           <h5 className="cider-name">Cider Name</h5>
           <p className="flavors">flavor/style in italics</p>
-          <p className="cider-award-year">awards and year of win</p>
+          <p className="cider-award-year">*awards and year of win</p>
         </div>
         <div className="cider-list-items">
           <h5 className="cider-name">Cider Name</h5>
           <p className="flavors">flavor/style in italics</p>
-          <p className="cider-award-year">awards and year of win</p>
+          <p className="cider-award-year">*awards and year of win</p>
         </div>
       </div>
     
-    <p>Might add awards here or give it its own component and page</p>
+    <h3>*Awards</h3>
     </div>
   )
 
