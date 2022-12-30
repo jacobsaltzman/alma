fbb0cf528e170a27bc019d43649baa63b72d5779
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2022-12-30 01:53:33 -0800

    Adds: images to the awards section and begins styling.

--- alma-app/src/components/Ciders.js
+++ alma-app/src/components/Ciders.js
@@ -1,5 +1,9 @@
 import React from "react";
 import AwardSlides from "./AwardSlideShow";
+import award from '/home/ickus/Development/code/alma/alma-app/src/images/award.jpg';
+import award2 from '/home/ickus/Development/code/alma/alma-app/src/images/fb_alma.jpg';
+import award3 from '/home/ickus/Development/code/alma/alma-app/src/images/award_facebook.jpg';
+
 
 function Ciders(){
 
@@ -35,7 +39,16 @@ function Ciders(){
       <h3>*Awards</h3>
       <AwardSlides/>
     </div>
+    <div id="awards-images">
+      <img alt="rose award" src={award}></img>
+      <img alt="hopped award" src={award3}></img>
+      <img alt="wood award" src={award2}></img>
+      
+    </div>
 
+    <div id="awards-links">
+      <a href="https://glintcap.org/results/commercial/medalists/alma-cider/">GLINTCAP</a>
+    </div>
     </div>
   )
 
